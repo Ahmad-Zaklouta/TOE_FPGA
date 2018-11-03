ca809262801ee7271fccc69996753316607b563e
Karol Marso <karci.marso@gmail.com>
2018-11-03 15:22:04 +0100

    Author: Karol Marso
    Subject: Created test environment for RX engine

--- src/RXEngine.vhd
+++ src/RXEngine.vhd
@@ -31,31 +31,38 @@ entity
 architecture rx_engine_behaviour of rx_engine is
   type state_t is (await, pseudo_header_read, header_read, read_data, wait_toe);
   type header_buffer_t is array(0 to 27) of std_ulogic_vector(7 downto 0);
-  signal tcp_header_reg: t_tcp_header;
+  
   signal header_buffer: header_buffer_t;
   signal state, state_next: state_t;
   signal count, count_next, header_count, header_count_next: std_ulogic_vector(15 downto 0);
   signal checksum, checksum_next: std_ulogic_vector(7 downto 0);
-  signal half_word, half_word_next: std_ulogic_vector(15 downto 0);
   signal byte2, byte2_next: std_ulogic; --forms two bit word
-  signal address_reg
+  
 begin
   comb: process(state, tvalid, tlast, tdata, i_forwardRX, i_discard)
   begin
     tready <= '1';
+	o_valid <= '0';
 	
     case state is
 	  when await =>
 	    if (tvalid = '1') then
-		  checksum_next <= std_ulogic_vector(unsigned(checksum) + unsigned(tdata));
+		  checksum_next <= checksum(7) xor tdata(7) & checksum(6) xor tdata(6) & checksum(5) xor tdata(5) & checksum(4) xor tdata(4) & checksum(3) xor tdata(3) & checksum(2) xor tdata(2) & checksum(1) xor tdata(1) & checksum(0) xor tdata(0) ;
+		  byte2_next <= not byte2;
 		  count_next <= std_ulogic_vector(unsigned(count) + 1);
 		  header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
           header_buffer(0) <= tdata
+		  state_next <= pseudo_header_read;
 		end if;
 	  when pseudo_header_read =>
 	    if (tvalid = '1') then
-		  checksum_next <= std_ulogic_vector(unsigned(checksum) + unsigned(tdata));
-	      if (unsigned(count) < 11) then
+		  byte2_next <= not byte2;
+		  if(byte2 = '0') then
+		    checksum_next <= checksum(7) xor tdata(7) & checksum(6) xor tdata(6) & checksum(5) xor tdata(5) & checksum(4) xor tdata(4) & checksum(3) xor tdata(3) & checksum(2) xor tdata(2) & checksum(1) xor tdata(1) & checksum(0) xor tdata(0) ;
+	      else
+		    checksum_next <= checksum(15) xor tdata(7) & checksum(14) xor tdata(6) & checksum(13) xor tdata(5) & checksum(12) xor tdata(4) & checksum(11) xor tdata(3) & checksum(10) xor tdata(2) & checksum(9) xor tdata(1) & checksum(8) xor tdata(0);
+		  end if;
+		  if (unsigned(count) < 11) then
 		    if(unsigned(header_count) < 7) then
 		      header_buffer(unsigned(header_count)) <= tdata;
 			  header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
@@ -64,26 +71,51 @@ begin
 			  header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
 			end if;  
 		  else
-		  
+		    state_next <= header_read;
 		  end if;
 		end if;
 	  when header_read =>
-	    if (unsigned(count < 31) then
-		  header_buffer(unsigned(header_count)) <= tdata;
-		  header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
-		else
-		  header_buffer(unsigned(header_count)) <= tdata;
-		  header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
+	    if(tvalid = '1') then  
+		    byte2_next <= not byte2;
+		    if(byte2 = '0') then
+		      checksum_next <= checksum(7) xor tdata(7) & checksum(6) xor tdata(6) & checksum(5) xor tdata(5) & checksum(4) xor tdata(4) & checksum(3) xor tdata(3) & checksum(2) xor tdata(2) & checksum(1) xor tdata(1) & checksum(0) xor tdata(0) ;
+	        else
+		      checksum_next <= checksum(15) xor tdata(7) & checksum(14) xor tdata(6) & checksum(13) xor tdata(5) & checksum(12) xor tdata(4) & checksum(11) xor tdata(3) & checksum(10) xor tdata(2) & checksum(9) xor tdata(1) & checksum(8) xor tdata(0);
+		    end if;
+		  if (unsigned(count < 31) then
+		    header_buffer(unsigned(header_count)) <= tdata;
+		    header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
+		  else
+		    header_buffer(unsigned(header_count)) <= tdata;
+		    header_count_next <= std_ulogic_vector(unsigned(header_count) + 1);
+			if(tlast = '1') then
+			  state_next <= wait_toe;
+		    else
+			  state_next <= read_data;
+			end if;
+		  end if;
 		end if;
 	  when read_data =>
-	    checksum_next <= std_ulogic_vector(unsigned(checksum) + unsigned(tdata));
-		if (tlast = '1') then
-		
-		else
-		
+	    if(tvalid = '1') then
+	      byte2_next <= not byte2;
+		  if(byte2 = '0') then
+		      checksum_next <= checksum(7) xor tdata(7) & checksum(6) xor tdata(6) & checksum(5) xor tdata(5) & checksum(4) xor tdata(4) & checksum(3) xor tdata(3) & checksum(2) xor tdata(2) & checksum(1) xor tdata(1) & checksum(0) xor tdata(0) ;
+	      else
+		      checksum_next <= checksum(15) xor tdata(7) & checksum(14) xor tdata(6) & checksum(13) xor tdata(5) & checksum(12) xor tdata(4) & checksum(11) xor tdata(3) & checksum(10) xor tdata(2) & checksum(9) xor tdata(1) & checksum(8) xor tdata(0);
+		  end if;
+		  if (tlast = '1') then
+		    state_next <= wait_toe;
+			tready <= '0';
+		  end if;
 		end if;
 	  when wait_toe =>
 	    --check the checksum here
+		tready <= '0';
+		if(i_forwardRX = '1') then
+		  state_next <= await;
+		elsif(i_discard = '1') then
+		  state_next <= await;
+		end if;
 	end case;
   end process;  
 
@@ -93,10 +125,14 @@ begin
 	  state <= await;
 	  count <= (others => '0');
 	  half_word <= (others => '0');
+	  byte2 <= '0';
+	  checksum <= (others => '0');
 	elsif (rising_edge(clk)
 	  state <= state_next;
 	  count <= count_next;
 	  half_word <= half_word_next;
+	  byte2 <= byte2_next;
+	  checksum <= checksum_next;
 	end if;
   end process;
 end rx_engine_behaviour;
---
+++ src/env_RX_test.vhd
@@ -0,0 +1,108 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+
+entity env is
+  port ( 
+    clk            : in std_logic; -- system clock
+    reset          : in std_logic; -- asynchronous reset
+	tvalid         : out std_logic;
+	tready         : in std_logic;
+	tdata          : out std_logic_vector(7 downto 0);
+	tlast          : out std_logic
+  );
+end env;
+
+architecture behavioural of env is
+  type state_t is (start, start_tx, send_data, prefinish, finish);
+  type FRAME_1 is array(0 to 31) of std_logic_vector(7 downto 0);
+  signal state, state_next : state_t;
+  constant frame : FRAME_1 := (X"0a",
+X"10",
+X"8c",
+X"9e",
+X"02",
+X"10",
+X"00",
+X"06",
+X"00",
+X"14",
+X"3f",
+X"41",
+X"dd",
+X"77",
+X"00",
+X"50",
+X"99",
+X"f3",
+X"1b",
+X"b2",
+X"b8",
+X"32",
+X"89",
+X"49",
+X"50",
+X"11",
+X"00",
+X"fd",
+X"01",
+X"ee",
+X"00",
+X"00"
+);
+  signal i, i_next: integer := 0;
+  signal data: std_logic_vector(7 downto 0);
+
+begin
+
+
+data <= frame(i);
+
+comb: process(i, state, data)
+begin
+   state_next <= state;
+   i_next <= i;
+   tvalid <= '1';
+   tdata <= data;
+   tlast <= '0';
+   
+   case state is
+      when start =>
+	    data_out <= (others => '0');
+	    state_next <= start_tx;
+	  when start_tx =>
+	    tvalid <= '1';
+		i_next <= i + 1;
+		state_next <= send_data;
+	  when send_data =>
+		i_next <= i + 1;
+		state_next <= send_data;
+	    tvalid <= '1';
+		if (i = 31) then
+		   i_next <= 0;
+		   tlast <= '1';
+		   state_next <= prefinish;
+		end if;
+	  when prefinish =>
+	    state_next <= finish;
+	  when finish =>
+	     --data_out <= (others => '0');
+	     --state_next <= finish;
+		 --assert fcs_error = '0' report "Error spotted" severity failure;
+		 --report "Test passed" severity failure;
+	end case;
+end process;
+
+seq: process(clk, reset)
+begin
+  if (reset = '1') then
+    state <= start;
+	i <= 0;
+  elsif (rising_edge(clk)) then
+    state <= state_next;
+	i <= i_next;
+  end if;
+end process;
+
+end behavioural;
