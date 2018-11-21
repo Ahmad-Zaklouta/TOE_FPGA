a5c90f2a6b0171ef87939a05041d6f898a5d9c3a
fb39ca4 <fb39ca4@gmail.com>
2018-11-21 01:10:45 +0100

    TX engine structure, still needs work

--- src/tcp_common.vhdl
+++ src/tcp_common.vhdl
@@ -7,12 +7,12 @@ package tcp_common is
 	subtype t_tcp_port is unsigned(15 downto 0);
 	subtype t_seq_num is unsigned(31 downto 0);
 	subtype t_byte is std_ulogic_vector(7 downto 0);
-	type t_tcp_header_buffer is array (19 downto 0)
+	--type t_tcp_header_buffer is array (19 downto 0);
 
 	type t_tcp_header is record
 		src_ip : t_ipv4_address;
 		dst_ip : t_ipv4_address;
-		length : unsigned(15 downto 0);
+		--length : unsigned(15 downto 0);
 		src_port : t_tcp_port;
 		dst_port : t_tcp_port;
 		seq_num : t_seq_num;
@@ -42,18 +42,19 @@ package tcp_common is
 	) return t_tcp_port;
 
 	constant c_default_tcp_header : t_tcp_header := (
-		src_ip <= ipv4_address(0, 0, 0, 0),
-		dst_ip <= ipv4_address(0, 0, 0, 0),
-		src_port <= tcp_port(0),
-		dst_port <= tcp_port(0),
-		seq_num <= (others => '0'),
-		ack_num <= (others => '0'),
-		data_offset <= (others => '0'),
-		reserved <= (others => '0'),
-		flags <= (others => '0'),
-		window_size <= (others => '0'),
-		checksum <= (others => '0'),
-		urgent_ptr <= (others => '0')
+		src_ip => (others => '0'),
+		dst_ip => (others => '0'),
+		--length => (others => '0'),
+		src_port => (others => '0'),
+		dst_port => (others => '0'),
+		seq_num => (others => '0'),
+		ack_num => (others => '0'),
+		data_offset => (others => '0'),
+		reserved => (others => '0'),
+		flags => (others => '0'),
+		window_size => (others => '0'),
+		checksum => (others => '0'),
+		urgent_ptr => (others => '0')
 	);
 
 end package tcp_common;
@@ -85,20 +86,45 @@ package body tcp_common is
 		header : t_tcp_header;
 		index : integer range 0 to 27
 	) return t_byte is
-		variable ret;
+		variable ret : std_ulogic_vector(7 downto 0);
 	begin
 		case index is
-			when 0 => ret := header.src_ip(31 downto 24);
-			when 1 => ret := header.src_ip(23 downto 16);
-			when 2 => ret := header.src_ip(15 downto  8);
-			when 3 => ret := header.src_ip( 7 downto  0);
+			when  0 => ret := header.src_ip(31 downto 24);
+			when  1 => ret := header.src_ip(23 downto 16);
+			when  2 => ret := header.src_ip(15 downto  8);
+			when  3 => ret := header.src_ip( 7 downto  0);
 
-			when 4 => ret := header.dst_ip(31 downto 24);
-			when 5 => ret := header.dst_ip(23 downto 16);
-			when 6 => ret := header.dst_ip(15 downto  8);
-			when 7 => ret := header.dst_ip( 7 downto  0);
-			-- WIP
+			when  4 => ret := header.dst_ip(31 downto 24);
+			when  5 => ret := header.dst_ip(23 downto 16);
+			when  6 => ret := header.dst_ip(15 downto  8);
+			when  7 => ret := header.dst_ip( 7 downto  0);
+
+			when  8 => ret := std_ulogic_vector(header.src_port(15 downto 8));
+			when  9 => ret := std_ulogic_vector(header.src_port( 7 downto 0));
+			when 10 => ret := std_ulogic_vector(header.dst_port(15 downto 8));
+			when 11 => ret := std_ulogic_vector(header.dst_port( 7 downto 0));
+
+			when 12 => ret := std_ulogic_vector(header.seq_num(31 downto 24));
+			when 13 => ret := std_ulogic_vector(header.seq_num(23 downto 16));
+			when 14 => ret := std_ulogic_vector(header.seq_num(15 downto  8));
+			when 15 => ret := std_ulogic_vector(header.seq_num( 7 downto  0));
+
+			when 16 => ret := std_ulogic_vector(header.ack_num(31 downto 24));
+			when 17 => ret := std_ulogic_vector(header.ack_num(23 downto 16));
+			when 18 => ret := std_ulogic_vector(header.ack_num(15 downto  8));
+			when 19 => ret := std_ulogic_vector(header.ack_num( 7 downto  0));
+
+			when 20 => ret := std_ulogic_vector(header.data_offset & "000" & header.flags(8));
+			when 21 => ret := header.flags(7 downto 0);
+			when 22 => ret := std_ulogic_vector(header.window_size(15 downto 8));
+			when 23 => ret := std_ulogic_vector(header.window_size( 7 downto 0));
+
+			when 24 => ret := header.checksum(15 downto 8);
+			when 25 => ret := header.checksum( 7 downto 0);
+			when 26 => ret := std_ulogic_vector(header.window_size(15 downto 8));
+			when 27 => ret := std_ulogic_vector(header.window_size( 7 downto 0));
 		end case;
+		return ret;
 	end function;
 
 end tcp_common;
